7a1cda9d916163c9aefc72c5a21d67d68800b35f
Marius <marius@example.com>
2022-11-11 09:03:01 +0200

    one Item

--- js/main.js
+++ js/main.js
@@ -1,2 +1,27 @@
 'use strict';
 console.log('main.js');
+
+class Shop {
+  // html element targets
+  el = {};
+  items = [];
+
+  constructor() {
+    this.initTargets();
+    this.getItems();
+  }
+
+  initTargets() {
+    this.el.list = document.getElementById('products');
+  }
+
+  getItems() {
+    getProducts().then((products) => {
+      this.items = products;
+      // console.log(JSON.stringify(products[0], null, 2));
+    });
+  }
+}
+
+const app = new Shop();
+console.log('app ===', app);

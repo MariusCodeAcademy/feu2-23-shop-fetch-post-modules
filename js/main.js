c75f30bea3bc701aaffdde4cf7f0bf90959e2141
Marius <marius@example.com>
2022-11-11 11:01:16 +0200

    form values

--- js/main.js
+++ js/main.js
@@ -3,3 +3,35 @@ console.log('main.js');
 
 const app = new Shop();
 console.log('app ===', app);
+
+class MyForm {
+  formEl = document.forms[0];
+
+  constructor() {
+    this.initListener();
+  }
+
+  initListener() {
+    this.formEl.addEventListener('submit', (event) => {
+      // sutabdyti forma nuo issiuntimo
+      event.preventDefault();
+      // iskonsoliti kazka
+      console.log('js control form');
+      // surinkti visus inputus i javaskriptini objekta
+      // ispausdinti objeka
+    });
+  }
+
+  getCategoriesArray(allDataArr) {
+    // is allDataArr === app.items
+    // atrinkti visas skirtingas kategorijas i masyva ir ji irasyti i this.categoriesArr
+  }
+
+  getCategoriesFetch() {
+    // gauti kategorijas is https://dummyjson.com/products/categories
+    // irasyti i this.categoriesArrFetch
+  }
+}
+
+const form1 = new MyForm();
+console.log('form1 ===', form1);
